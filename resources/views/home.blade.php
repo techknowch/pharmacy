e931804eded6c838496a994f03dfe86eb5428747
techknowch <techknowch@protonmail.com>
2023-09-02 14:18:47 +0500

    02/09/2023
    2:18 pm

---
+++ resources/views/home.blade.php
@@ -0,0 +1,154 @@
+@extends('layouts.app')
+
+@section('content')
+            <div class="page-content">
+                <div class="container-fluid">
+
+                                 <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                <h4 class="mb-sm-0">Widgets</h4>
+
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Velzon</a></li>
+                                        <li class="breadcrumb-item active">Widgets</li>
+                                    </ol>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div class="row">
+                        <div class="col-12">
+                            <h5 class="text-decoration-underline mb-3 pb-1">Tile Boxs</h5>
+                        </div>
+                    </div>
+                    <!-- end row-->
+
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1">
+                                            <p class="text-uppercase fw-medium text-muted mb-0">Total Earnings</p>
+                                        </div>
+                                        <div class="flex-shrink-0">
+                                            <h5 class="text-success fs-14 mb-0">
+                                                <i class="ri-arrow-right-up-line fs-13 align-middle"></i> +16.24 %
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value" data-target="559.25">0</span>k</h4>
+                                            <a href="#" class="text-decoration-underline">View net earnings</a>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-soft-success rounded fs-3">
+                                                <i class="bx bx-dollar-circle text-success"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate bg-info">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1">
+                                            <p class="text-uppercase fw-medium text-white-50 mb-0">Orders</p>
+                                        </div>
+                                        <div class="flex-shrink-0">
+                                            <h5 class="text-warning fs-14 mb-0">
+                                                <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4 text-white"><span class="counter-value" data-target="36894">0</span></h4>
+                                            <a href="#" class="text-decoration-underline text-white-50">View all orders</a>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-soft-light rounded fs-3">
+                                                <i class="bx bx-shopping-bag text-white"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1">
+                                            <p class="text-uppercase fw-medium text-muted mb-0">Customers</p>
+                                        </div>
+                                        <div class="flex-shrink-0">
+                                            <h5 class="text-success fs-14 mb-0">
+                                                <i class="ri-arrow-right-up-line fs-13 align-middle"></i> +29.08 %
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="183.35">0</span>M</h4>
+                                            <a href="#" class="text-decoration-underline">See details</a>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-soft-warning rounded fs-3">
+                                                <i class="bx bx-user-circle text-warning"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1">
+                                            <p class="text-uppercase fw-medium text-muted mb-0">My Balance</p>
+                                        </div>
+                                        <div class="flex-shrink-0">
+                                            <h5 class="text-muted fs-14 mb-0">
+                                                +0.00 %
+                                            </h5>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">$<span class="counter-value" data-target="165.89">0</span>k</h4>
+                                            <a href="#" class="text-decoration-underline">Withdraw money</a>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-soft-primary rounded fs-3">
+                                                <i class="bx bx-wallet text-primary"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+                    </div> <!-- end row-->
+
+                </div>
+                <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
+@endsection
